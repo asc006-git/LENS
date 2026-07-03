e83e871b17f236b6c8e728236c03726db25b148f
asc006-git <adhyatmasinghchauhan20031@gmail.com>
2026-07-03 18:06:02 +0530

    chore: config & styling - globals.css theme tokens, layout provider, Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
